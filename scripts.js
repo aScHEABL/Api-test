c83938ffa1f72fb5b8ff1e2a93047d8d33f4cf47
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-11-08 15:22:08 +0800

    update: add quatation mark

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ btn.addEventListener("click", (e) => {
 })
 
 function getTheGif () {
-    fetch("https://api.giphy.com/v1/gifs/translate?api_key=A28osyX29BlXhy00UYwX2Y1tMISNBsUb&s=slap&weirdness=10", {mode: "cors"})
+    fetch("https://api.giphy.com/v1/gifs/translate?api_key=A28osyX29BlXhy00UYwX2Y1tMISNBsUb&s=\"slap\"&weirdness=10", {mode: "cors"})
     .then((response) => {
         return response.json();
     })
